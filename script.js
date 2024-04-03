c5c669b007afcffd151c0187a1e27543fd8b11c0
21PM <parasmore33@gmail.com>
2024-04-03 15:10:52 +0530

    Minor changes done in chart section

--- script.js
+++ script.js
@@ -7,16 +7,47 @@ var inputAmount =  document.getElementById("amount");
 var addBtn = document.getElementById("Add_Button");
 var defaultSelected  =  document.getElementById("default");
 
-// var activitesDiv =  document.getElementById("activites_div");
 
 
-// function DeleteHistory(event){
-//     if (event.target.classList.contains("material-symbols-outlined") && event.target.id === "deleteBtn") {
-//         const deleteBtn = event.target;
-//         const transactionDiv = deleteBtn.parentElement; // Get the parent transaction div
-//         transactionDiv.style.display = "none"; // Hide the transaction div
-//       }
-// }
+
+
+
+const ctx = document.getElementById('myChart');
+
+const chart = new Chart(ctx, 
+  {
+  type: 'bar',
+  data: {
+    labels: ['Total Salary', 'Expenses', 'Available Balance'],
+    datasets: [{
+      label: 'Expenses chart',
+      backgroundColor : ["pink"],
+      data: [],   
+      backgroundColor: [
+    'green',
+    'red',
+    'gold',
+    
+  ],
+     
+
+
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+// chart.update();
+
+
+console.log(chart.data.datasets[0].data[2]);
+
 
 
 // console.log(total_salary.firstElementChild.innerText);
@@ -26,14 +57,27 @@ category.addEventListener('change',function(e){
     // console.log(e);
     var selectedValue = category.value;
 
-    // console.log(selectedValue);
-
     addBtn.removeEventListener('click', handleButtonClick); // Remove previous click event listener to avoid multiple bindings
 
     
-
+    
     function handleButtonClick() {
+
+        if(inputAmount.value == "" || inputTitle.value ==""){
+            alert("Fill all the details");
+        }
+
+
         if (selectedValue === "income") {
+
+
+            if(inputAmount.value == "" || inputTitle.value ==""){
+                alert("Fill all the details");
+                
+            }else  if(inputAmount.value != "" || inputTitle.value !=""){
+    
+
+            
             // console.log("Income selected");
 
             var totalNum =  parseInt(total_salary.innerText);
@@ -55,15 +99,12 @@ category.addEventListener('change',function(e){
                 <span class="material-symbols-outlined" id="deleteBtn">delete </span>
 
             `;
-            
-        
+
             // Append the new div to the activitiesDiv
             activitesDiv.appendChild(transactionDiv);  
             
             //******************* */ Ending Transaction History********************
             
-
-        
             
             selectedValue = "";
             inputAmount.value = "";  inputTitle.value = "";
@@ -71,10 +112,17 @@ category.addEventListener('change',function(e){
 
 
             AvailableBalanceCheck(); //Subtract the salary from expenses and show the available balance
+        }
             
             
         } 
         else if (selectedValue === "expenses") {
+
+
+            if(inputAmount.value == "" || inputTitle.value ==""){
+                alert("Fill all the details");
+                
+            }else if(inputAmount.value != "" || inputTitle.value !=""){
 
             var totalExp =  parseInt(expense_pay.innerText);
             totalExp += parseInt(inputAmount.value);  
@@ -105,8 +153,8 @@ category.addEventListener('change',function(e){
             category.selectedIndex = 0;
 
             AvailableBalanceCheck(); // Subtract the salary from expenses and show the available balance
-
-
+            }
+            
         }
 
     }
@@ -116,36 +164,7 @@ category.addEventListener('change',function(e){
     // Add click event listener to the button
     addBtn.addEventListener('click', handleButtonClick);
 
-    
-    
-  
-
 })
-
-
-
-// var activites_div =  document.querySelectorAll(".Transaction_history");
-
-// activites_div.forEach((item) => {
-//     var deleteBtn = document.querySelector("#deleteBtn");
-//     // console.log(deleteBtn.parentElement);
-
-
-//     deleteBtn.addEventListener('click',DeleteHistory)
-
-//     function  DeleteHistory(){
-        
-//         console.log("delted function called");
-//         deleteBtn.parentElement.style.display = "none";
-        
-
-
-//     }
-
-// });
-
-
-
 
 
 function AvailableBalanceCheck(e){
@@ -153,11 +172,24 @@ function AvailableBalanceCheck(e){
     var totalsal = parseInt(total_salary.innerText);
     var totalExpe =  parseInt(expense_pay.innerText);
     var Balance =  document.getElementById("You_get_back");
+
+   
+    chart.data.datasets[0].data[0] = totalsal;
+    chart.data.datasets[0].data[1] = totalExpe;
+    chart.data.datasets[0].data[2] = Balance;
+
+      chart.update();
+
       
 
     if(totalsal >  totalExpe){
         
         var balanceCal = String(totalsal - totalExpe);
+
+        // chart.data.datasets[0].data[0] = parseInt(balanceCal); 
+
+
+        // chart.update();
 
         Balance.innerText = balanceCal;
 
@@ -166,15 +198,27 @@ function AvailableBalanceCheck(e){
 
         var balanceCal = parseInt(totalsal - totalExpe);
 
+        chart.data.datasets[0].data[1] = parseInt(balanceCal); 
+
+        // chart.update();
+
         // console.log(balanceCal);
 
         Balance.innerText = (balanceCal);
 
     }else if(totalExpe === totalsal){
 
+
+      // chart.data.datasets[0].data[0] = parseInt(balanceCal); 
+
+
         Balance.innerText =  00;
     }
 }
+
+
+
+
 
 
 
