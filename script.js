a5051376a67e92f97c7582169beda3533e6b65c4
21PM <parasmore33@gmail.com>
2024-04-04 22:38:05 +0530

    Payment integration done tested successfully

--- script.js
+++ script.js
@@ -295,8 +295,36 @@ function calculateTax() {
      console.log(PayableTax);
 
      console.log(typeof PayableTax);
-}
 
+     var options = {
+      "key": "rzp_test_YNXeqP1kSxE19T", // Enter the Key ID generated from the Dashboard
+      "amount": PayableTax, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+      "currency": "INR",
+      "name": "Acme Corp", //your business name
+      "description": "Test Transaction",
+      "image": "https://example.com/your_logo",
+      // "order_id": "order_9A33XWu170gUtm", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+      "callback_url": "https://eneqd3r9zrjok.x.pipedream.net/",
+      "prefill": { //We recommend using the prefill parameter to auto-fill customer's contact information especially their phone number
+          "name": "Gaurav Kumar", //your customer's name
+          "email": "gaurav.kumar@example.com",
+          "contact": "9000090000" //Provide the customer's phone number for better conversion rates 
+      },
+      "notes": {
+          "address": "Razorpay Corporate Office"
+      },
+      "theme": {
+          "color": "#3399cc"
+      }
+    };
+    var rzp1 = new Razorpay(options);
+    document.getElementById('rzp-button1').onclick = function(e){
+      rzp1.open();
+      e.preventDefault();
+    }
+    
+}
+// console.log(PayableTax);
 
 
 
@@ -327,32 +355,6 @@ function CloseCalPayTax(){
 /// *************************************payment integration started from here ***********************************
 
 
-var options = {
-  "key": "rzp_test_YNXeqP1kSxE19T", // Enter the Key ID generated from the Dashboard
-  "amount": PayableTax, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
-  "currency": "INR",
-  "name": "Acme Corp", //your business name
-  "description": "Test Transaction",
-  "image": "https://example.com/your_logo",
-  // "order_id": "order_9A33XWu170gUtm", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
-  "callback_url": "https://eneqd3r9zrjok.x.pipedream.net/",
-  "prefill": { //We recommend using the prefill parameter to auto-fill customer's contact information especially their phone number
-      "name": "Gaurav Kumar", //your customer's name
-      "email": "gaurav.kumar@example.com",
-      "contact": "9000090000" //Provide the customer's phone number for better conversion rates 
-  },
-  "notes": {
-      "address": "Razorpay Corporate Office"
-  },
-  "theme": {
-      "color": "#3399cc"
-  }
-};
-var rzp1 = new Razorpay(options);
-document.getElementById('rzp-button1').onclick = function(e){
-  rzp1.open();
-  e.preventDefault();
-}
 
 
 
